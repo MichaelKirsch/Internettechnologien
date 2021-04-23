8a10a987c5838fbc2aeac68fcb2277eb1b983eba
Michael <michael_kirsch@t-online.de>
2021-04-23 18:15:20 +0200

    addded nice markers for my map

--- app.py
+++ app.py
@@ -1,30 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, RadioField
+from wtforms.validators import DataRequired
+from config import Config
+
 import db
 import sqlite3
 app = Flask(__name__)
+app.config.from_object(Config)
 
 db.init_app(app)
 
-
-
-data=[
-    {
-        'id' : '1',
-        'lat':'47.71423318561046',
-        'lon': '10.310717989923432',
-        'location':'Kempten',
-        'type' : 'Automat',
-    }
-]
+class LoginForm(FlaskForm):
+    lat = StringField(validators=[DataRequired()])
+    lon = StringField(validators=[DataRequired()])
+    type = RadioField('Typ', choices=[('A','Automat'),('V','Verkaufstelle'),('B','Verkaufstelle + Automat')])
+    description = StringField("Beschreibung",validators=[DataRequired()])
+    location = StringField("Standort",validators=[DataRequired()])
+    submit = SubmitField('Eröffnen')
+data =[]
 
 
 @app.route('/')
 def index():
     return render_template("uebersicht.html")
 
-@app.route('/eroeffnen')
+@app.route('/eroeffnen', methods=['GET', 'POST'])
 def eroeffnen():
-    return render_template("eroeffnen.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        l = request.form.get('lat')
+        o = request.form.get('lon')
+        t = request.form.get('type')
+        des = request.form.get('description')
+        loc = request.form.get('location')
+
+        x = db.get_db()
+
+        cur = x.cursor()
+        try:
+            querey = ("INSERT INTO station (coordsA, coordsL, location, type, description) VALUES ('{}','{}','{}','{}','{}') ".format(l,o,loc,t,des))
+            print(querey)
+            cur.execute(querey)
+            x.commit()
+        finally:
+            cur.close()
+        return redirect(url_for('liste'))
+    return render_template("eroeffnen.html", title='Sign In', form=form)
 
 @app.route('/liste')
 def liste():
--- static/js/eroeffnen.js
+++ static/js/eroeffnen.js
@@ -13,7 +13,9 @@ var marker = L.marker([47.721053, 10.307946])//.addTo(map)
 function onMapClick(e){
     marker.setLatLng(e.latlng)
     marker.addTo(map)
-    document.getElementById("akt_position").innerHTML = "Aktuell gewählt | Lat:"+e.latlng.lat + " Lon:" +e.latlng.lng
+    document.getElementById("lat").value = e.latlng.lat
+    document.getElementById("lon").value = e.latlng.lng
+    document.getElementById("gewaehlter").innerHTML = e.latlng.lat+"|"+e.latlng.lng
     console.log(e.latlng)
 }
 
